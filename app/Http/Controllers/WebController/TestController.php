536d1e64c29c7999c9843211ecba608be38911ba
WRBKOR23 <haijdk2433@gmail.com>
2021-08-31 16:23:21 +0700

    test

--- app/Http/Controllers/WebController/TestController.php
+++ app/Http/Controllers/WebController/TestController.php
@@ -1,38 +1,39 @@
 <?php
 
-    namespace App\Http\Controllers\WebController;
+namespace App\Http\Controllers\WebController;
 
-    use App\BusinessClass\CrawlQLDTData;
-    use App\Helpers\SharedFunctions;
-    use App\Http\Controllers\Controller;
-    use App\Models\Account;
-    use App\Models\DataVersionStudent;
-    use App\Models\Department;
-    use App\Models\Faculty;
-    use App\Models\ModuleClass;
-    use App\Models\ModuleScore;
-    use App\Models\Notification;
-    use App\Models\NotificationAccount;
-    use App\Models\OtherDepartment;
-    use App\Models\Participate;
-    use App\Models\Schedule;
-    use App\Models\Teacher;
-    use Illuminate\Http\Request;
-    use Illuminate\Support\Facades\Cache;
-    use Illuminate\Support\Facades\DB;
-    use Illuminate\Support\Facades\Storage;
+use App\BusinessClass\CrawlQLDTData;
+use App\Helpers\SharedFunctions;
+use App\Http\Controllers\Controller;
+use App\Models\Account;
+use App\Models\DataVersionStudent;
+use App\Models\Department;
+use App\Models\Faculty;
+use App\Models\ModuleClass;
+use App\Models\ModuleScore;
+use App\Models\Notification;
+use App\Models\NotificationAccount;
+use App\Models\OtherDepartment;
+use App\Models\Participate;
+use App\Models\Schedule;
+use App\Models\Teacher;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
-    class TestController extends Controller
+class TestController extends Controller
+{
+    public function test ()
     {
-        public function test ()
-        {
 //            $a = new DataVersionStudent();
 //            Cache::put('a', $a->get('191201402'), 5);
 ////            var_dump(Cache::get('a'));
 //
 //            return Cache::get('a');
-            var_dump(dirname(storage_path()));
-            var_dump(config('filesystems.disks.errors.file_path'));
-            return ;
-        }
+//            var_dump(dirname(storage_path()));
+//            var_dump(config('filesystems.disks.errors.file_path'));
+        return nl2br(File::get(dirname(storage_path()) . '/bootstrap/cache/config.php'));
     }
+}
